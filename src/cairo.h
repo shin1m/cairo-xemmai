f16631f45d66a277e51d5a9b79522914cb2b6df2
Shin-ichi MORITA <shin1morita@gmail.com>
2023-05-07 06:53:51 +0900

    Removed explicit use of Boolean.

--- src/cairo.h
+++ src/cairo.h
@@ -323,16 +323,7 @@ struct t_holds : t_instantiatable<t_bears<T>>
 		static bool f_call(T1&& a_object)
 		{
 			auto p = t_base::f_object(std::forward<T1>(a_object));
-			switch (reinterpret_cast<uintptr_t>(p)) {
-			case e_tag__NULL:
-				return true;
-			case e_tag__BOOLEAN:
-			case e_tag__INTEGER:
-			case e_tag__FLOAT:
-				return false;
-			default:
-				return p->f_type()->template f_derives<typename t_fundamental<T0>::t_type>();
-			}
+			return reinterpret_cast<uintptr_t>(p) == e_tag__NULL || reinterpret_cast<uintptr_t>(p) >= e_tag__OBJECT && p->f_type()->template f_derives<typename t_fundamental<T0>::t_type>();
 		}
 	};
 	using t_library = xemmaix::cairo::t_library;
